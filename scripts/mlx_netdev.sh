c7d4f899fe09cca27a0733743f646e20fc6a504b
Haggai Eran <haggai.eran@gmail.com>
2019-12-10 17:30:40 +0200

    scripts: work with standard path for mlx_ip/mlx_netdev scripts

--- scripts/mlx_netdev.sh
+++ scripts/mlx_netdev.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# fpga_mst_dev=$(/usr/bin/ls /dev/mst/*fpga_i2c)
+# Find the first ConnectX device with an IP assigned.
+# TODO find an Innova device
+
+# fpga_mst_dev=$(ls /dev/mst/*fpga_i2c)
 # cx_mst_dev=${fpga_mst_dev%%_fpga_i2c}
 
-# bdf=$(sudo mst status | /usr/bin/grep -Pzo "${cx_mst_dev}.*\n.*domain:bus:dev.fn=\K(.*)(?= addr)")
-# netdev=$(/usr/bin/ls -d /sys/bus/pci/devices/$bdf/net/* | /usr/bin/xargs -n1 /usr/bin/basename)
+# bdf=$(sudo mst status | grep -Pzo "${cx_mst_dev}.*\n.*domain:bus:dev.fn=\K(.*)(?= addr)")
+# netdev=$(ls -d /sys/bus/pci/devices/$bdf/net/* | xargs -n1 basename)
 # echo $netdev
 
-candidates=$(/usr/bin/ls -d /sys/bus/pci/drivers/mlx5_core/*/net/* | /usr/bin/xargs -n1 /usr/bin/basename)
+candidates=$(ls -d /sys/bus/pci/drivers/mlx5_core/*/net/* | xargs -n1 basename)
 declare -a ether
 ether=()
-for netdev in $candidates ; do
-    if /usr/sbin/ip link show $netdev | /usr/bin/grep ether > /dev/null ; then
+for netdev in ${candidates[@]} ; do
+    if ip link show $netdev | grep ether > /dev/null ; then
         ether+=($netdev)
     fi
 done
 declare -a has_ip
 has_ip=()
-for netdev in $ether ; do
-    if /usr/sbin/ip addr show dev $netdev | /usr/bin/grep inet > /dev/null ; then
+for netdev in ${ether[@]} ; do
+    if ip addr show dev $netdev | grep inet > /dev/null ; then
         has_ip+=($netdev)
     fi
 done
