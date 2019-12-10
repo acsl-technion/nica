55bdd2c5d2bf5f173ea2c85c915f18f0ca491d4a
Haggai Eran <haggai.eran@gmail.com>
2019-12-10 17:31:02 +0200

    scripts: allow multiple devices when disabling credits

--- scripts/disable-mellanox-shell-credits.sh
+++ scripts/disable-mellanox-shell-credits.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Find FPGA Innova netdev
-dev=/dev/mst/mt4117_pciconf0_fpga_i2c
+devs=$(ls /dev/mst/mt4117_pciconf*_fpga_i2c)
 
 echo Disabling Mellanox shell credits
 
 function set_credits_enable() {
-    cur=$(sudo mlx_fpga -d $dev r $1 | grep -v 'Unsupported SBU ID')
-    sudo mlx_fpga -d $dev w $1 $[$cur | (1 << 15)] | grep -v 'Unsupported SBU ID'
+    local dev="$1"
+    local reg="$2"
+    cur=$(sudo mlx_fpga -d $dev r $reg | grep -v 'Unsupported SBU ID')
+    sudo mlx_fpga -d $dev w $reg $[$cur | (1 << 15)] | grep -v 'Unsupported SBU ID'
 }
 
-# .cxp.prt_config.prt_config_buffers.rxb_config.fifo0_rxb_credits_enable
-set_credits_enable 0x990004
-# .nwp.prt_config.prt_config_buffers.rxb_config.fifo0_rxb_credits_enable
-set_credits_enable 0x9b0004
+for dev in $devs ; do
+    # .cxp.prt_config.prt_config_buffers.rxb_config.fifo0_rxb_credits_enable
+    set_credits_enable $dev 0x990004
+    # .nwp.prt_config.prt_config_buffers.rxb_config.fifo0_rxb_credits_enable
+    set_credits_enable $dev 0x9b0004
+done
