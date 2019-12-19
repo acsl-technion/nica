f4b9c73ca12ddd7f393367ac21a75bcc7b83197f
Haggai Eran <haggaie@mellanox.com>
2019-12-19 14:22:42 +0200

    scripts: set MTU to 1522 in the shell to support Innova IPSec cards

--- scripts/disable-mellanox-shell-credits.sh
+++ scripts/disable-mellanox-shell-credits.sh
@@ -18,3 +18,23 @@ for dev in $devs ; do
     # .nwp.prt_config.prt_config_buffers.rxb_config.fifo0_rxb_credits_enable
     set_credits_enable $dev 0x9b0004
 done
+
+function set_mtu() {
+    local dev="$1"
+    local reg="$2"
+    local mtu="$3"
+
+    cur=$(sudo mlx_fpga -d $dev r $reg | grep -v 'Unsupported SBU ID')
+    printf "cur = 0x%x\n" $cur
+    new=$[($cur & ~((1 << 14) - 1)) | $mtu]
+    printf "new = 0x%x\n" $new
+    sudo mlx_fpga -d $dev w $reg $new | grep -v 'Unsupported SBU ID'
+}
+
+echo Setting shell MTU to 1522
+for dev in $devs ; do
+    # cxp.prt_config.prt_config_common.mtu_frame_size
+    set_mtu $dev 0x99201c 1522
+    # .nwp.prt_config.prt_config_common.mtu_frame_size
+    set_mtu $dev 0x9b201c 1522
+done
